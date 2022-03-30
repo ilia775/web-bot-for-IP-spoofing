29017ec03e57c8fa2b7c869864291185067ce64e
ilia775 <53534936+ilia775@users.noreply.github.com>
2022-03-30 23:13:53 +0300

    Add files via upload

---
+++ GetProxyList.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def GetProxyList():
+    url = r'https://www.sslproxies.org/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    Table = soup.findAll('table')[0].tbody.findAll('td')
+    AllProxy = ''
+    for i in range(0, len(Table), 8):
+        row = Table[i]
+        AllProxy += row.text + ':'
+        i += 1
+        row = Table[i]
+        if i + 8 <= len(Table):
+            AllProxy += row.text + ' '
+        else:
+            AllProxy += row.text
+    AllProxy = AllProxy.split(' ')
+    return AllProxy
+
+
+print(GetProxyList())
