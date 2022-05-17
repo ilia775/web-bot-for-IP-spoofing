f63225d935796672cfa56bc4a36cde2d3e3fb343
ilia775 <53534936+ilia775@users.noreply.github.com>
2022-05-17 21:05:42 +0300

    Update main.py
    добавили юзер агенты

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from bs4 import BeautifulSoup
 import aiohttp
 import requests
 import os
+from fake_useragent import UserAgent
+import random
 
-proxy_type = r"http://"
-test_url = r"https://min-api.cryptocompare.com/"
+proxy_type = "http://"
+test_url = 'http://example.com'
 timeout_sec = 4
 GoodProxiesHTTP = []
 GoodProxiesHTTPS = []
@@ -15,15 +17,36 @@ AllProxyHTTP = []
 AllProxyHTTPS = []
 CurrentProxy = 0
 
+class Proxy:
+    def __init__(self, ip:str, port:str, https:str):
+        self.ip = ip
+        self.port = port
+        self.is_https = https == 'yes'
 
-def getallproxy():
+    def __str__(self):
+        return f"{'https' if self.is_https else 'http'}://{self.ip}:{self.port}"
+
+
+def getallproxy(proxy_list):
     urls = ["https://www.socks-proxy.net/", "https://free-proxy-list.net/", "https://www.us-proxy.org/", "https://free-proxy-list.net/uk-proxy.html", "https://www.sslproxies.org/", "https://free-proxy-list.net/anonymous-proxy.html"]
     #urls = ["https://www.sslproxies.org/", "https://free-proxy-list.net/", "https://free-proxy-list.net/anonymous-proxy.html"]
     for url in urls:
-        getproxy(url)
+        getproxy(url, proxy_list)
 
 
-def getproxy(url):
+def getproxy(url, proxy_list):
+     response = requests.get(url)
+     soup = BeautifulSoup(response.content, 'html.parser')
+     for table in soup.find_all('table'):
+             if 'table-striped' in table['class']:
+                 tempt = table.find_all('td')
+                 #for i in [ [el.text for el in tempt[i:i+8]] for i in range(0,len(tempt),8)]:
+                      #print(str(Proxy(i[0],i[1], i[6]))) 
+                 #return [ str(Proxy(x[0], x[1], x[6])) for x in [ [el.text for el in tempt[i:i+8]] for i in range(0,len(tempt),8)]]
+                 for i in [ [el.text for el in tempt[i:i+8]] for i in range(0,len(tempt),8)]:
+                     proxy_list.append(Proxy(i[0], i[1], i[6]))
+
+def getproxy1(url):
 
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -45,25 +68,24 @@ def getproxy(url):
 
 async def checkproxyhttp(ipport):
      try:
-         ipport = "http://" + ipport;
+         #ipport = "http://" + ipport;
          session = aiohttp.ClientSession()
-         resp = await session.get(test_url, proxy=ipport, timeout=timeout_sec)
-         #print(1)
+         resp = await session.get(test_url, proxy=ipport, headers = {'User-Agent' : UserAgent().random}, timeout=timeout_sec)
          await session.close()
          #print(resp.status)
          if resp.status != 200:
             raise "Error"
          GoodProxiesHTTP.append(ipport[7:])
      except:
-         #print(2)
+         print(2)
          await session.close()
 
 
-def getdata(Proxies, CurrentProxy):
+def getdata(Proxy):
     #Proxies[CurrentProxy] = {"http": Proxies[CurrentProxy]};
     try:
         s = requests.session();
-        s.proxies = {"http":Proxies[CurrentProxy]}
+        s.proxies = {"http":Proxy}
    
         resp = s.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=RUB,USD,JPY,EUR")
         if r"You are over your rate limit please upgrade your account!" in resp.text:
@@ -73,53 +95,56 @@ def getdata(Proxies, CurrentProxy):
         print(resp.text)
     except:
         print(0)
-        print(Proxies[CurrentProxy])
+        print(Proxy)
         return -1
 
 
 
 
-def checkproxy():
+def checkproxy(proxy_list):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     tasks = []
-    for item in AllProxyHTTP:
-        tasks.append(asyncio.ensure_future(checkproxyhttp(item)))
+    for item in proxy_list:
+        if item.is_https == 0:
+            tasks.append(asyncio.ensure_future(checkproxyhttp(str(item))))
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
 
+if __name__ == "__main__":
 
-timer = time.time()
-getallproxy()
-print(AllProxyHTTPS)
-print(AllProxyHTTP)
-checkproxy()
-print(GoodProxiesHTTP)
-t = time.time()
-timeout = 60
-CurrentProxy = 0
-while True:
-    if time.time() - t > timeout:
-        print("timeout")
-        print(time.time() - t)
-        GoodProxiesHTTP = []
-        AllProxy = []
-        getallproxy()
-        checkproxy()
-        CurrentProxy = 0
-        t = time.time()
-
-    if len(GoodProxiesHTTP) == 0:
-        t -= timeout
-        continue
-    if getdata(GoodProxiesHTTP, CurrentProxy) == -1:
-        CurrentProxy += 1
-        #t -= timeout
-    if CurrentProxy >= len(GoodProxiesHTTP):
-        t -= timeout
-        continue
+    timer = time.time()
+    proxy_list = []
+    getallproxy(proxy_list)
+    #print(len(proxy_list))
+    checkproxy(proxy_list)
     print(GoodProxiesHTTP)
-    print(time.time() - timer)
+    t = time.time()
+    timeout = 60
+    CurrentProxy = 0
+    while True:
+        if time.time() - t > timeout:
+            print("timeout")
+            print(time.time() - t)
+            proxy_list = []
+            GoodProxiesHTTP = []
+            getallproxy(proxy_list)
+            checkproxy(proxy_list)
+            CurrentProxy = 0
+            t = time.time()
+
+        if len(GoodProxiesHTTP) == 0:
+            t -= timeout
+            print("pizda")
+            continue
+        if getdata(str(GoodProxiesHTTP[CurrentProxy])) == -1:
+            CurrentProxy += 1
+            #t -= timeout
+        if CurrentProxy >= len(proxy_list):
+            t -= timeout
+            continue
+        #print(GoodProxiesHTTP)
+        print(time.time() - timer)
 
 
 
